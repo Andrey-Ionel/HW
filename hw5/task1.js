1e08cd6a4a164f280269d4a0221f0bbdd0a3673f
Andrey-Ionel <a-ionel@ukr.net>
2020-12-24 19:07:51 +0200

    all tasks is ended

--- hw5/task1.js
+++ hw5/task1.js
@@ -3,5 +3,50 @@
 // Затем, удалить элементы из массива с 2 по 4 (если длина меньше 4 вывести сообщение). 
 // По мере изменений, выводить содержимое массива на страницу.
 
-const anyNumber = prompt('Please enter any number:', 0);
+const arrayOfValue = [];
+let lenghtOfArray = arrayOfValue.length;
+lenghtOfArray = parseInt(prompt('Please enter the lenght of the array from 1 to 20.', 1));
 
+if (isNaN(lenghtOfArray)) {
+    alert('Incorrect input');
+} else {
+    if (lenghtOfArray === 0) {
+        alert('Cancel');
+    } else if (lenghtOfArray > 20) {
+        alert('Array is too long.');
+    } else {
+        const lenghtResult = `The lenght of the array is '${lenghtOfArray}'.`
+        console.log(lenghtResult);
+        const falResulte = `The lenght of the array is '0'.`
+        for (i = 0; i < lenghtOfArray; i++) {
+            arrayOfValue[i] = String(prompt('Please enter array value ' + (i + 1)));
+            if (arrayOfValue[i] === '' || arrayOfValue[i] === ' ') {
+                alert('Incorrect input');
+                arrayOfValue.splice(0, lenghtOfArray);
+                console.log(falResulte);
+                break;
+            } else if (arrayOfValue[i] === 'null') {
+                alert('Cancel');
+                arrayOfValue.splice(0, lenghtOfArray);
+                console.log(falResulte);
+                break;
+            } else {
+                const resultOfCreating = `Result ${arrayOfValue[i]} array is `
+                console.log(resultOfCreating);
+                console.log(arrayOfValue);
+            }
+        }
+    }
+};
+const sortResult = `The array has been sorted`
+console.log(sortResult);
+console.log(arrayOfValue.sort());
+if (lenghtOfArray < 4) {
+    const deleteResult = `Cannot delete elements from '2 to 4' because the array lenght is less then '4'.`
+    console.log(deleteResult);
+} else {
+    arrayOfValue.splice(1, 3);
+    const resultOfDeletedElements = `Elements from '2 to 4' have been removed`
+    console.log(resultOfDeletedElements);
+}
+console.log(arrayOfValue);
