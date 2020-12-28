17db8149f89488d89ce9023417f3478f48cce5c1
Andrey-Ionel <a-ionel@ukr.net>
2020-12-29 01:37:42 +0200

    add new task

--- hw6/task1.js
+++ hw6/task1.js
@@ -11,6 +11,7 @@
 // т.е. если пользователь ввел "1 ыва 2 56", то калькуляцию мы должны провести тольк с 1 2 56
 
 // Для этого у массива есть специальный метод который позволяет убрать ненужные значения.
+
 const mathOperation = prompt('Please, enter math operation');
 
 if (chackIfCanceledOperation()) {
@@ -19,7 +20,51 @@ if (chackIfCanceledOperation()) {
     alert('Incorrect input');
 } else {
     createOperationLog();
-    createArrayOfNumbers();
+
+    const arrayOfNumber = createArrayOfNumbers();
+
+    const filteredArrayOfNumber = arrayOfNumber.filter(function (item, index, array) {
+        if (item === '') {
+            return false;
+        } else if (isNaN(item)) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    function createFilteredArrayLog() {
+        filteredArrayOfNumber === true;
+        console.log(`Your array of numbers is [${filteredArrayOfNumber}].`);
+        return true;
+    };
+    createFilteredArrayLog();
+
+    performsArrayOperations();
+
+    function performsArrayOperations() {
+
+        if (mathOperation === '+') {
+            const sumOfOperation = filteredArrayOfNumber.reduce((acc, value) => parseInt(acc) + parseInt(value));
+            return sumOfOperation;
+        } else if (mathOperation === '-') {
+            const minustOfOperation = filteredArrayOfNumber.reduce((acc, value) => acc - value);
+            return minustOfOperation;
+        } else if (mathOperation === '/') {
+            const divisionOfOperation = filteredArrayOfNumber.reduce((acc, value) => acc / value);
+            return divisionOfOperation;
+        } else if (mathOperation === '*') {
+            const multiplicationOfOperation = filteredArrayOfNumber.reduce((acc, value) => acc * value);
+            return multiplicationOfOperation;
+
+        }
+    };
+    function createresultLog() {
+        performsArrayOperations() === true;
+        console.log(`The result of your operation is ${performsArrayOperations()}.`);
+        return true;
+    };
+    createresultLog();
 };
 
 
@@ -28,29 +73,27 @@ function chackIfCanceledOperation() {
         return true;
     }
 };
+
 function checkCorrectInput() {
     if (mathOperation !== '+' && mathOperation !== '-' && mathOperation !== '*' && mathOperation !== '/') {
         return true;
     };
 };
 
+
 function createOperationLog() {
     mathOperation === true;
     console.log(`Your operation is (${mathOperation}).`);
     return true;
 };
-const arrayOfNumber = [];
+
 function createArrayOfNumbers() {
     const enterNumbers = prompt('Please enter numbers separated by a space');
     if (enterNumbers === null) {
         alert('Cancel input');
         return true;
-
     } else {
         const arrayOfNumber = enterNumbers.split(' ');
-        console.log(arrayOfNumber);
-        return true;
-    };
-}
-
-
+        return arrayOfNumber;
+    }
+};
