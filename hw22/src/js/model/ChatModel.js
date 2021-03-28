22da8eb894130337995173b79f5be78bf4339cc5
Ionel-Andrey <andrey.ivanov688@gmail.com>
2021-03-28 20:50:21 +0300

    hw22

---
+++ hw22/src/js/model/ChatModel.js
@@ -0,0 +1,41 @@
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+export class ChatModel {
+    constructor() {
+        this.$newMessage = this.newMessage;
+    }
+
+    async addChatData(message, author) {
+        const socket = new WebSocket('ws://andreychatserve.herokuapp.com/');
+
+        socket.onopen = function () {
+            const sendMessageEvent = {
+                type: 'SEND_MESSAGE',
+                payload: {
+                    author: `${author}`,
+                    message: `${message}`,
+                },
+            };
+
+            const json = JSON.stringify(sendMessageEvent);
+
+            socket.send(json);
+        };
+
+        socket.onmessage = (event) => {
+            const newMessage = event.data;
+            this.newMessage = newMessage;
+            return this.newMessage;
+        };
+
+        socket.onerror = (event) => {
+            console.log('onerror', event);
+        };
+
+        socket.onclose = (event) => {
+            console.log('onclose', event);
+        };
+    }
+}
