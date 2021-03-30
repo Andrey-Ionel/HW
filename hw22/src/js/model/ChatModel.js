417343911a0067df6083f7eadca026851415c18e
Ionel-Andrey <andrey.ivanov688@gmail.com>
2021-03-31 02:26:04 +0300

    all complete

--- hw22/src/js/model/ChatModel.js
+++ hw22/src/js/model/ChatModel.js
@@ -3,40 +3,22 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
 export class ChatModel {
-    constructor() {
+    constructor(config) {
+        this.config = config;
+        this.socket = new WebSocket('ws://localhost:7777');
 
-    }
-
-    async addChatData(message, author) {
-        const socket = new WebSocket('ws://localhost:7777');
-
-        socket.onopen = function () {
-            const sendMessageEvent = {
-                type: 'SEND_MESSAGE',
-                payload: {
-                    author: `${author}`,
-                    message: `${message}`,
-                },
-            };
-
-            const json = JSON.stringify(sendMessageEvent);
-
-            socket.send(json);
-        };
-
-        socket.onmessage = (event) => {
+        this.socket.onmessage = (event) => {
             const newMessage = JSON.parse(event.data);
-            console.log(newMessage.payload.message);
-            newMessage.payload.author = author;
-            newMessage.payload.message = message;
-            return { message, author };
+            this.author = newMessage.payload.author;
+            this.message = newMessage.payload.message;
+            this.config.getChatData(this.message, this.author);
         };
 
-        socket.onerror = (event) => {
+        this.socket.onerror = (event) => {
             console.log('onerror', event);
         };
 
-        socket.onclose = (event) => {
+        this.socket.onclose = (event) => {
             console.log('onclose', event);
         };
     }
